1b9b20a461043b30a8fcd90e17405ad4fe03e0be
arshspencer <arshspencer.digibrain@gmail.com>
2019-05-07 16:05:33 -0400

    add handlers

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class TodoInput extends Component {
   render() {
     return (
-        <h1>Hello from todo input</h1>
+        <h1>Hello from Todo Input</h1>
     )
   }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,9 @@ import Item from './TodoItem';
 
 export default class TodoList extends Component {
   render() {
-    return (<div>
-        <h1>Hello From TodoList</h1>
+    return (
+        <div>
+        <h1>Hello From Todo List</h1>
         <Item/>
         </div>
     )
