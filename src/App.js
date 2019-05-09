80ef4a0ffeae0b3034b173c96954f57929373855
arshspencer <arshspencer.digibrain@gmail.com>
2019-05-09 08:03:17 -0400

    project complete

--- src/App.js
+++ src/App.js
@@ -4,20 +4,55 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 class App extends Component {
-  state={
-    items:[{id:1,title:"wake up"},{id:2,title:"make breakfast"}],
-    id:uuid(),
-    editItem:false,
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false
   };
-    handleChange=(e)=>{console.log('handle change')}
-    handleSubmit=(e)=>{console.log('handle change')}
-    clearList=(e)=>{console.log('clear list')}
-    handleDelete=(id)=>{console.log(`handle delete ${id}`)}
-    handleEdit=(id)=>{console.log(`handle edit ${id}`)}
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
+  };
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+  handleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
+  };
+  handleEdit = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    });
+  };
 
   render() {
-    console.log(this.state);
-    
     return (
       <div className="container">
         <div className="row">
@@ -27,8 +62,8 @@ class App extends Component {
             </h3>
             <TodoInput 
             item={this.state.item}
-            handleChange={this.state.handleChange}
-            handleSubmit={this.state.handleSubmit}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
             editItem={this.state.editItem}/>
             <TodoList
             items={this.state.items}
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -4,23 +4,26 @@ export default class TodoInput extends Component {
   render() {
 
     const {item,handleChange,handleSubmit,editItem} = this.props;
+  
     return (
       <div className="card card-body my-3">
-        <from onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <div className="input-group-prepend">
               <div className="input-group-text bg-primary text-white">
                 <i className="fas fa-book"/>
-              </div>
+                </div>
+                  </div>
+                    <input type="text" 
+                    className="form-control text-capitalize" 
+                    placeholder="add todo-item"
+                    value={item}
+                    onChange={handleChange}
+                    />
+                    
             </div>
-            <input type="text" 
-            className="form-control text-capitalize" 
-            placeholder="add todo-item"
-            value={item}
-            onChange={handleChange}/>
-          </div>
-          <button type="submit" className="btn btn-block btn-primary mt-3 text-uppercase">Add Items</button>
-        </from>
+          <button type="submit" disabled={item ? false:true} className={editItem ? "btn btn-block btn-success mt-3 text-uppercase":"btn btn-block btn-primary mt-3 text-uppercase"}>{editItem ? 'edit item' : 'add item'}</button>
+        </form>
       </div>
     )
   }
